81f1a1ba0b8121df1a64bf756f802cfbf0dc5ace
rocoide <roodrigo4@hotmail.com>
2024-02-25 19:55:32 -0300

    Add project files.

---
+++ TeAyudo_Microservicio_Acompanante/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace TeAyudo_Microservicio_Acompanante
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
